078957bef55b5918deaeb75c15cfec0f9c9a8bd3
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2023-11-17 17:22:56 -0300

    config.js

--- config.js
+++ config.js
@@ -2,13 +2,12 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAkp3gQdHbMPD5pCfHbBzIIgFpdCJXD5KM",
-  authDomain: "wily-app-v2.firebaseapp.com",
-  projectId: "wily-app-v2",
-  storageBucket: "wily-app-v2.appspot.com",
-  messagingSenderId: "772559744213",
-  appId: "1:772559744213:web:9417b879882f231175065c",
-  measurementId: "G-H8Y352W6Z2"
+  apiKey: "AIzaSyAh0jprnhSK-67Z9T3zXtrDJbG3y_sftl8",
+  authDomain: "biblioteca-eletronica-14cc5.firebaseapp.com",
+  projectId: "biblioteca-eletronica-14cc5",
+  storageBucket: "biblioteca-eletronica-14cc5.appspot.com",
+  messagingSenderId: "285652837628",
+  appId: "1:285652837628:web:05d31bc21ab36a31726379"
 };
 
 firebase.initializeApp(firebaseConfig);
